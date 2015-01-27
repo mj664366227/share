b54feaf321f70c3443639807d88958f7ec7cbe7d
ruanzhijun <ruan965176201@qq.com>
2015-01-27 22:27:08 +0800

    xx

---
+++ java/share-core/src/java/com/share/core/interfaces/BaseHandler.java
@@ -0,0 +1,15 @@
+package com.share.core.interfaces;
+
+import org.apache.log4j.Logger;
+
+/**
+ * handler基类
+ * @author ruan
+ *
+ */
+public abstract class BaseHandler {
+	/**
+	 * logger
+	 */
+	protected Logger logger = Logger.getLogger(getClass());
+}
