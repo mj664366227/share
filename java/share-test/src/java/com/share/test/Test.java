ea0b14054ad6c5caf93397a77be84d2c220c99eb
ruanzhijun <ruanzhijun@kugou.net>
2015-02-05 15:56:45 +0800

    xx

--- java/share-test/src/java/com/share/test/Test.java
+++ java/share-test/src/java/com/share/test/Test.java
@@ -2,36 +2,9 @@ package com.share.test;
 
 import java.io.IOException;
 
-import com.google.gson.reflect.TypeToken;
-import com.share.core.util.JSONObject;
-
 public class Test {
 	public static void main(String[] args) throws IOException {
-		String json = "{\"a\":\"fdfdfd\"}";
-		A a = JSONObject.decode(json, new TypeToken<A>() {
-		}.getType());
+		float a = 999999999933333f;
 		System.err.println(a);
-	}
-
-}
-
-class A {
-	private String a;
-	private int b;
-
-	public String getA() {
-		return a;
-	}
-
-	public void setA(String a) {
-		this.a = a;
-	}
-
-	public int getB() {
-		return b;
-	}
-
-	public void setB(int b) {
-		this.b = b;
 	}
 }
