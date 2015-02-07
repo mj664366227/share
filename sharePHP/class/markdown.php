dd15d59e13c7473b97a218d6aaf7a9b447962d13
ruanzhijun <ruan965176201@qq.com>
2015-02-07 15:24:15 +0800

    xx

--- sharePHP/class/markdown.php
+++ sharePHP/class/markdown.php
@@ -6,7 +6,7 @@ class markdown{
 	/**
 	 * 输出的html代码(js部分与依赖jquery，请自行引入)
 	 */
-	private $html = '<div style="width:100%" id="md_file_content"><style>h1,h2,h3,h4,h5,h6{margin:20px;font-weight:bold}h1{font-size:32px}h2{font-size:28px}h3{font-size:24px}h4{font-size:20px}h5{font-size:16px}h6{font-size:16px}.anchor{display:none;margin-top:5px;margin-right:5px;float:left;width:18px;height:10px;background:url(\'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABIAAAAKCAIAAAA2KZn2AAAABmJLR0QA/wD/AP+gvaeTAAAACXBIWXMAAA7EAAAOxAGVKw4bAAAAuklEQVQokZ2RLRKDMBSElypukNwAHJKjgEMiOUZwHKG9QXCVSCQSHLYO3MNtRRvolHb6sybzzbx9b2fjkcTvOtzfZajLNNTeJh2mZT3M83BK9YqL85GktCZ+szY2rUhTqAckSZDsqghAnNtRuElGm8cAoqrbI5wrs9NtvDMKQH4WUppi85GTzRwe9rl8DcD3V74s+5kXIb/J/FyJKhr50JCrhCSltyYJFACVHPtpWtFJBYmxvbvv/ffdVycYutMLAImQAAAAAElFTkSuQmCC\') no-repeat}</style>';
+	private $html = '<div style="width:100%" id="md_file_content"><style>*{color:#000}h1,h2,h3,h4,h5,h6{margin:20px;font-weight:bold}h1{font-size:32px}h2{font-size:28px}h3{font-size:24px}h4{font-size:20px}h5{font-size:16px}h6{font-size:16px}p{margin-left:45px}.anchor{display:none;margin-top:5px;margin-right:5px;float:left;width:18px;height:10px;background:url(\'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABIAAAAKCAIAAAA2KZn2AAAABmJLR0QA/wD/AP+gvaeTAAAACXBIWXMAAA7EAAAOxAGVKw4bAAAAuklEQVQokZ2RLRKDMBSElypukNwAHJKjgEMiOUZwHKG9QXCVSCQSHLYO3MNtRRvolHb6sybzzbx9b2fjkcTvOtzfZajLNNTeJh2mZT3M83BK9YqL85GktCZ+szY2rUhTqAckSZDsqghAnNtRuElGm8cAoqrbI5wrs9NtvDMKQH4WUppi85GTzRwe9rl8DcD3V74s+5kXIb/J/FyJKhr50JCrhCSltyYJFACVHPtpWtFJBYmxvbvv/ffdVycYutMLAImQAAAAAElFTkSuQmCC\') no-repeat}</style>';
 	/**
 	 * 文件句柄
 	 */
@@ -97,12 +97,13 @@ class markdown{
 	 * @param $buffer 文件流
 	 */
 	private function parse_content($buffer){
-		$bool = $this->is_title($buffer);
-		$bool = $this->is_hr($buffer);
+		$bool = true;
+		$bool = $bool && !$this->is_title($buffer);
+		$bool = $bool && !$this->is_hr($buffer);
 		if(!$bool){
 			return;
 		}
-		echo_($buffer);
+		$this->html .= '<p>'.$buffer.'</p>';
 	}
 }
 ?>
