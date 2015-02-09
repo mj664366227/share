4c20cc698ca1b4c819be7cb8916ad8b91a379738
ruanzhijun <ruanzhijun@kugou.net>
2015-02-09 13:50:27 +0800

    xx

--- java/share-core/src/java/com/share/core/util/SortUtil.java
+++ java/share-core/src/java/com/share/core/util/SortUtil.java
@@ -7,16 +7,19 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * 通用排序类
+ * @param <V>
  */
-public final class SortUtil {
+public final class SortUtil<K, V> {
 	/**
 	 * logger
 	 */
@@ -24,6 +27,7 @@ public final class SortUtil {
 	/**
 	 * 装载已经用过的规则 实现类似单例模式
 	 */
+	@SuppressWarnings("rawtypes")
 	private static Map<String, SortUtil> sortMap = new HashMap<String, SortUtil>();
 	/**
 	 * 方法名数组
@@ -37,6 +41,10 @@ public final class SortUtil {
 	 * 排序方式
 	 */
 	private Order order = null;
+	/**
+	 * 根据什么排序
+	 */
+	private OrderBy orderBy = null;
 
 	/**
 	 * 构造函数 并保存该规则
@@ -59,11 +67,21 @@ public final class SortUtil {
 	}
 
 	/**
-	 * 构造函数 ，设置排序规则
-	 * @param order
+	 * 构造函数
+	 * @param order 排序规则
 	 */
 	private <T> SortUtil(Order order) {
 		this.order = order;
+	}
+
+	/**
+	 * 构造函数
+	 * @param order 排序规则
+	 * @param orderBy 根据排序
+	 */
+	private <T> SortUtil(Order order, OrderBy orderBy) {
+		this.order = order;
+		this.orderBy = orderBy;
 	}
 
 	/**
@@ -77,70 +95,7 @@ public final class SortUtil {
 				for (int i = 0; i < methodArr.length; i++) {
 					Object value1 = methodArr[i].invoke(o1);
 					Object value2 = methodArr[i].invoke(o2);
-					if (value1 instanceof Integer) {
-						int int1 = Integer.parseInt(value1.toString());
-						int int2 = Integer.parseInt(value2.toString());
-						if (int1 > int2) {
-							result = 1;
-						} else if (int1 < int2) {
-							result = -1;
-						}
-					} else if (value1 instanceof Boolean) {
-						boolean boolean1 = Boolean.parseBoolean(value1.toString());
-						boolean boolean2 = Boolean.parseBoolean(value2.toString());
-						if (boolean1 && !boolean2) {
-							result = 1;
-						} else if (!boolean1 && boolean2) {
-							result = -1;
-						}
-					} else if (value1 instanceof Double) {
-						double double1 = Double.parseDouble(value1.toString());
-						double double2 = Double.parseDouble(value2.toString());
-						if (double1 > double2) {
-							result = 1;
-						} else if (double1 < double2) {
-							result = -1;
-						}
-					} else if (value1 instanceof Float) {
-						float float1 = Float.parseFloat(value1.toString());
-						float float2 = Float.parseFloat(value2.toString());
-						if (float1 > float2) {
-							result = 1;
-						} else if (float1 < float2) {
-							result = -1;
-						}
-					} else if (value1 instanceof Long) {
-						long long1 = Long.parseLong(value1.toString());
-						long long2 = Long.parseLong(value2.toString());
-						if (long1 > long2) {
-							result = 1;
-						} else if (long1 < long2) {
-							result = -1;
-						}
-					} else if (value1 instanceof Short) {
-						short short1 = Short.parseShort(value1.toString());
-						short short2 = Short.parseShort(value2.toString());
-						if (short1 > short2) {
-							result = 1;
-						} else if (short1 < short2) {
-							result = -1;
-						}
-					} else if (value1 instanceof Byte) {
-						byte byte1 = Byte.parseByte(value1.toString());
-						byte byte2 = Byte.parseByte(value2.toString());
-						if (byte1 > byte2) {
-							result = 1;
-						} else if (byte1 < byte2) {
-							result = -1;
-						}
-					} else {
-						int rs = value1.toString().compareToIgnoreCase(value2.toString());
-						if (rs > 0) {
-							result = 1;
-						} else if (rs < 0) {
-							result = -1;
-						}
-					}
+					result = SortUtil.compare(value1, value2);
 					if (result == 0) {
 						continue;
 					}
@@ -159,39 +114,73 @@ public final class SortUtil {
 	private final Comparator<Object> comparatorValue = new Comparator<Object>() {
 		@Override
 		public int compare(Object o1, Object o2) {
+			int result = SortUtil.compare(o1, o2);
+			return order.equals(Order.ASC) ? result : -result;
+		}
+	};
+
+	/**
+	 * map排序规则
+	 */
+	private final Comparator<Entry<K, V>> comparatorMap = new Comparator<Entry<K, V>>() {
+		public int compare(Entry<K, V> a1, Entry<K, V> a2) {
+			if (orderBy == null) {
+				return 0;
+			}
 			int result = 0;
-			if (o1 instanceof Integer) {
-				result = Integer.parseInt(o1.toString()) >= Integer.parseInt(o2.toString()) ? 1 : -1;
-			} else if (o1 instanceof Boolean) {
-				boolean boolean1 = Boolean.parseBoolean(o1.toString());
-				boolean boolean2 = Boolean.parseBoolean(o2.toString());
-				if (boolean1 && !boolean2) {
-					result = 1;
-				} else if (!boolean1 && boolean2) {
-					result = -1;
-				}
-			} else if (o1 instanceof Double) {
-				result = Double.parseDouble(o1.toString()) >= Double.parseDouble(o2.toString()) ? 1 : -1;
-			} else if (o1 instanceof Float) {
-				result = Float.parseFloat(o1.toString()) >= Float.parseFloat(o2.toString()) ? 1 : -1;
-			} else if (o1 instanceof Long) {
-				result = Long.parseLong(o1.toString()) >= Long.parseLong(o2.toString()) ? 1 : -1;
-			} else if (o1 instanceof Short) {
-				result = Short.parseShort(o1.toString()) >= Short.parseShort(o2.toString()) ? 1 : -1;
-			} else if (o1 instanceof Byte) {
-				result = Byte.parseByte(o1.toString()) >= Byte.parseByte(o2.toString()) ? 1 : -1;
-			} else {
-				result = o1.toString().compareToIgnoreCase(o2.toString()) >= 0 ? 1 : -1;
+			switch (orderBy) {
+			case KEY:
+				result = SortUtil.compare(a1.getKey(), a2.getKey());
+				break;
+			case VALUE:
+				result = SortUtil.compare(a1.getValue(), a2.getValue());
+				break;
+			default:
+				return 0;
 			}
 			return order.equals(Order.ASC) ? result : -result;
 		}
 	};
 
 	/**
+	 * 比较两个值的大小
+	 * @param o1
+	 * @param o2
+	 */
+	private final static int compare(Object o1, Object o2) {
+		int result = 0;
+		if (o1 instanceof Integer) {
+			result = Integer.parseInt(o1.toString()) >= Integer.parseInt(o2.toString()) ? 1 : -1;
+		} else if (o1 instanceof Boolean) {
+			boolean boolean1 = Boolean.parseBoolean(o1.toString());
+			boolean boolean2 = Boolean.parseBoolean(o2.toString());
+			if (boolean1 && !boolean2) {
+				result = 1;
+			} else if (!boolean1 && boolean2) {
+				result = -1;
+			}
+		} else if (o1 instanceof Double) {
+			result = Double.parseDouble(o1.toString()) >= Double.parseDouble(o2.toString()) ? 1 : -1;
+		} else if (o1 instanceof Float) {
+			result = Float.parseFloat(o1.toString()) >= Float.parseFloat(o2.toString()) ? 1 : -1;
+		} else if (o1 instanceof Long) {
+			result = Long.parseLong(o1.toString()) >= Long.parseLong(o2.toString()) ? 1 : -1;
+		} else if (o1 instanceof Short) {
+			result = Short.parseShort(o1.toString()) >= Short.parseShort(o2.toString()) ? 1 : -1;
+		} else if (o1 instanceof Byte) {
+			result = Byte.parseByte(o1.toString()) >= Byte.parseByte(o2.toString()) ? 1 : -1;
+		} else {
+			result = o1.toString().compareTo(o2.toString()) >= 0 ? 1 : -1;
+		}
+		return result;
+	}
+
+	/**
 	 * 获取排序规则
 	 * 
 	 * @return SortUtil
 	 */
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	private final static <T> SortUtil getSort(Class<T> clazz, String... args) {
 		String key = clazz.getName() + Arrays.toString(args);
 		SortUtil sort = sortMap.get(key);
@@ -209,11 +198,30 @@ public final class SortUtil {
 	 * @param order
 	 * @return
 	 */
+	@SuppressWarnings("rawtypes")
 	private final static <T> SortUtil getSort(Class<T> clazz, Order order) {
 		String key = clazz.getName() + order;
 		SortUtil sort = sortMap.get(key);
 		if (sort == null) {
 			sort = new SortUtil(order);
+			sortMap.put(key, sort);
+		}
+		return sort;
+	}
+
+	/**
+	 * 获取排序规则
+	 * @param clazz	
+	 * @param order
+	 * @param orderBy
+	 * @return
+	 */
+	@SuppressWarnings("rawtypes")
+	private final static <K, V> SortUtil getSort(Map<K, V> map, Order order, OrderBy orderBy) {
+		String key = map.toString() + order + orderBy;
+		SortUtil sort = sortMap.get(key);
+		if (sort == null) {
+			sort = new SortUtil(order, orderBy);
 			sortMap.put(key, sort);
 		}
 		return sort;
@@ -232,6 +240,7 @@ public final class SortUtil {
 	 * @param list
 	 * @param args
 	 */
+	@SuppressWarnings({ "unchecked", "rawtypes" })
 	public final static <T> void sort(List<T> list, String... args) {
 		SortUtil sort = getSort(list.get(0).getClass(), args);
 		Collections.sort(list, sort.comparatorObject);
@@ -243,6 +252,7 @@ public final class SortUtil {
 	 * @param list 数组
 	 * @param order 排序方式
 	 */
+	@SuppressWarnings({ "unchecked", "rawtypes" })
 	public final static <T> void sort(List<T> list, Order order) {
 		SortUtil sort = getSort(list.get(0).getClass(), order);
 		Collections.sort(list, sort.comparatorValue);
@@ -283,19 +293,22 @@ public final class SortUtil {
 	}
 
 	/**
-	 * 给map的key排序
-	 * @author ruan
+	 * 根据map的value排序(非对象)
 	 * @param map 被排序的map
 	 * @param order 排序方式
-	 * @return
+	 * @param orderBy 根据什么排序(key or value)	
 	 */
-	public final static <K, V> List<K> sortMapKey(Map<K, V> map, Order order) {
-		ArrayList<K> list = new ArrayList<K>();
-		for (K key : map.keySet()) {
-			list.add(key);
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public final static <K, V> Map<K, V> sortMap(Map<K, V> map, Order order, OrderBy orderBy) {
+		ArrayList<Entry<K, V>> list = new ArrayList<Entry<K, V>>(map.entrySet());
+		SortUtil sort = getSort(map, order, orderBy);
+		Collections.sort(list, sort.comparatorMap);
+		Map<K, V> newMap = new LinkedHashMap<K, V>();
+		int size = list.size();
+		for (int i = 0; i < size; i++) {
+			newMap.put(list.get(i).getKey(), list.get(i).getValue());
 		}
-		sort(list, order);
-		return list;
+		return newMap;
 	}
 
 	/**
@@ -313,5 +326,21 @@ public final class SortUtil {
 		 * 反序
 		 */
 		DESC;
+	}
+
+	/**
+	 * 根据什么来排序
+	 * @author ruan
+	 * 
+	 */
+	public enum OrderBy {
+		/**
+		 * 根据key来排序
+		 */
+		KEY,
+		/**
+		 * 根据value来排序
+		 */
+		VALUE;
 	}
 }
--- java/share-test/src/java/com/share/test/Test.java
+++ java/share-test/src/java/com/share/test/Test.java
@@ -1,10 +1,8 @@
 package com.share.test;
 
-import java.io.IOException;
-
 public class Test {
-	public static void main(String[] args) throws IOException {
-		float a = 999999999933333f;
-		System.err.println(a);
+	public static void main(String[] args) throws Exception {
+
 	}
+
 }
