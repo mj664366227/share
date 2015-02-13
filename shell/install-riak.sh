4faf919d6516c2d68ceb3b194999f9b1a51a55ae
ruanzhijun <ruan965176201@qq.com>
2015-02-13 09:36:18 +0800

    xx

--- shell/install-riak.sh
+++ shell/install-riak.sh
@@ -5,14 +5,6 @@
 base_path=$(pwd)
 ntpdate time.nist.gov
 
-yum -y install libtool sed gcc gcc-c++ make net-snmp net-snmp-devel net-snmp-utils libc6-dev python-devel rsync perl bc lrzsz openssh-server postfix cronie vim-enhanced readline readline-devel ncurses-devel gdbm-devel glibc-devel tcl-devel openssl-devel curl-devel expat-devel db4-devel byacc sqlite-devel libyaml libyaml-devel libffi libffi-devel libxml2 libxml2-devel libxslt libxslt-devel libicu libicu-devel system-config-firewall-tui python-devel crontabs logwatch logrotate perl-Time-HiRes autoconf 
-
-#建立临时安装目录
-echo 'preparing working path...'
-install_path='/install'
-rm -rf $install_path
-mkdir -p $install_path
-
 #处理外部参数
 riak_install_path=$1
 if [ ! $riak_install_path ]; then
@@ -20,6 +12,14 @@ if [ ! $riak_install_path ]; then
 	echo 'for example: sh install-riak.sh /usr/local'
 	exit
 fi
+
+yum -y install libtool sed gcc gcc-c++ make net-snmp net-snmp-devel net-snmp-utils libc6-dev python-devel rsync perl bc lrzsz openssh-server postfix cronie vim-enhanced readline readline-devel ncurses-devel gdbm-devel glibc-devel tcl-devel openssl-devel curl-devel expat-devel db4-devel byacc sqlite-devel libyaml libyaml-devel libffi libffi-devel libxml2 libxml2-devel libxslt libxslt-devel libicu libicu-devel system-config-firewall-tui python-devel crontabs logwatch logrotate perl-Time-HiRes autoconf 
+
+#建立临时安装目录
+echo 'preparing working path...'
+install_path='/install'
+rm -rf $install_path
+mkdir -p $install_path
 
 #安装erlang
 erlang='17.4'
