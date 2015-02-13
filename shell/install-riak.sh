f112ab7b0b575036edd68dbd59684c4763f4f956
ruanzhijun <ruan965176201@qq.com>
2015-02-13 09:32:19 +0800

    xx

---
+++ shell/install-riak.sh
@@ -0,0 +1,36 @@
+#linux git
+#运行例子：sh install-riak.sh /usr/local
+
+#定义本程序的当前目录
+base_path=$(pwd)
+ntpdate time.nist.gov
+
+yum -y install libtool sed gcc gcc-c++ make net-snmp net-snmp-devel net-snmp-utils libc6-dev python-devel rsync perl bc lrzsz openssh-server postfix cronie vim-enhanced readline readline-devel ncurses-devel gdbm-devel glibc-devel tcl-devel openssl-devel curl-devel expat-devel db4-devel byacc sqlite-devel libyaml libyaml-devel libffi libffi-devel libxml2 libxml2-devel libxslt libxslt-devel libicu libicu-devel system-config-firewall-tui python-devel crontabs logwatch logrotate perl-Time-HiRes autoconf 
+
+#建立临时安装目录
+echo 'preparing working path...'
+install_path='/install'
+rm -rf $install_path
+mkdir -p $install_path
+
+#处理外部参数
+riak_install_path=$1
+if [ ! $riak_install_path ]; then
+	echo 'error command!!! you must input riak install path...'
+	echo 'for example: sh install-riak.sh /usr/local'
+	exit
+fi
+
+#安装erlang
+erlang='17.4'
+if [ ! -d $riak_install_path/erlang ]; then 
+	echo 'installing erlang '$erlang'...'
+	if [ ! -f $base_path/otp_src_$erlang.tar.gz ]; then
+		echo 'otp_src_'$erlang'.tar.gz is not exists, system will going to download it...'
+		wget -O $base_path/otp_src_$erlang.tar.gz http://www.erlang.org/download/otp_src_$erlang.tar.gz || exit
+		echo 'download erlang'$erlang' finished...'
+	fi
+	tar zxvf $base_path/otp_src_$erlang.tar.gz -C $install_path || exit
+	cd $install_path/otp_src_$erlang
+	./configure --prefix=$riak_install_path/erlang && make && sudo make install
+fi
