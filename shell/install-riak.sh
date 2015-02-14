b5be39f9cd6ab2d4bd305f57cc0d920c0df78145
ruanzhijun <ruan965176201@qq.com>
2015-02-14 22:23:32 +0800

    xx

--- shell/install-riak.sh
+++ shell/install-riak.sh
@@ -21,7 +21,7 @@ rm -rf $install_path
 mkdir -p $install_path
 
 #安装erlang
-sh install-erlang.sh $riak_install_path 'R16B03'
+sh install-erlang.sh $riak_install_path 'R14B04'
 
 #安装riak
 riak='1.4.2'
