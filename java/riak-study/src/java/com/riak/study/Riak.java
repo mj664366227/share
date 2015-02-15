afa4f3694f6e2ebc265a5f8f5023aec3be9af9ec
ruanzhijun <ruan965176201@qq.com>
2015-02-15 16:54:05 +0800

    xx

--- java/riak-study/src/java/com/riak/study/Riak.java
+++ java/riak-study/src/java/com/riak/study/Riak.java
@@ -1,6 +1,23 @@
 package com.riak.study;
 
+import com.basho.riak.client.api.RiakClient;
+import com.basho.riak.client.api.cap.Quorum;
+import com.basho.riak.client.api.commands.kv.StoreValue;
+import com.basho.riak.client.api.commands.kv.StoreValue.Option;
+import com.basho.riak.client.core.query.Location;
+import com.basho.riak.client.core.query.Namespace;
+import com.basho.riak.client.core.query.RiakObject;
+import com.basho.riak.client.core.util.BinaryValue;
+
 public class Riak {
-	public static void main(String[] a) {
+	public static void main(String[] a) throws Exception {
+		RiakClient client = RiakClient.newClient("192.168.190.129");
+
+		Namespace ns = new Namespace("default", "my_bucket");
+		Location location = new Location(ns, "my_key");
+		RiakObject riakObject = new RiakObject();
+		riakObject.setValue(BinaryValue.create("my_value"));
+		StoreValue store = new StoreValue.Builder(riakObject).withLocation(location).withOption(Option.W, new Quorum(3)).build();
+		client.execute(store);
 	}
 }
---
+++ java/riak-study/src/java/com/riak/study/UserInfo.java
@@ -0,0 +1,40 @@
+package com.riak.study;
+
+public class UserInfo {
+	private String uid;
+	private String name;
+	private String city;
+	private String nickName;
+
+	public String getUid() {
+		return uid;
+	}
+
+	public void setUid(String uid) {
+		this.uid = uid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getNickName() {
+		return nickName;
+	}
+
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+}
