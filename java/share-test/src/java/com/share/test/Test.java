8947a12b535f47d09644f25799413b3249501c79
ruanzhijun <ruanzhijun@kugou.net>
2015-02-28 09:13:17 +0800

    xx

--- java/share-test/src/java/com/share/test/Test.java
+++ java/share-test/src/java/com/share/test/Test.java
@@ -2,7 +2,8 @@ package com.share.test;
 
 public class Test {
 	public static void main(String[] args) throws Exception {
-
+		String content = "可很快就会a";
+		System.err.println(content.length());
 	}
 
 }
