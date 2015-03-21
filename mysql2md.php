a3af79896f9d820cfe37e8435ebc3da5f00e2ce4
ruanzhijun <ruanzhijun@kugou.net>
2015-03-21 09:05:19 +0800

    xx

--- mysql2md.php
+++ mysql2md.php
@@ -1,30 +1,43 @@
 <?php
 
-mysql2md('localhost', 'root', 'root', 'share', 'D:\nginx\www\share.git\shareTools\md\mysql2md.md');
+mysql2md('192.168.0.152', 'root', 'kugou', 'kugou_sing', "F:\work\doc.git\sing\dataDictionary\kugou_sing.md");
+mysql2md('192.168.0.152', 'root', 'kugou', 'kugou_sing_admin', "F:\work\doc.git\sing\dataDictionary\kugou_sing_admin.md");
+mysql2md('192.168.0.152', 'root', 'kugou', 'kugou_sing_consume', "F:\work\doc.git\sing\dataDictionary\kugou_sing_consume.md");
+mysql2md('192.168.0.152', 'root', 'kugou', 'kugou_sing_log', "F:\work\doc.git\sing\dataDictionary\kugou_sing_log.md");
+mysql2md('192.168.0.152', 'root', 'kugou', 'kugou_sing_second', "F:\work\doc.git\sing\dataDictionary\kugou_sing_second.md");
 
 function mysql2md($host, $user, $pass, $db, $file){	
 	$conn = mysql_connect($host, $user, $pass);
 	mysql_query('set names utf8', $conn);
 	mysql_query('use '.$db, $conn);
 	
-	$data = '#'.$db."库字典表\r\n";
+	$data = '# '.$db."库字典表\r\n";
 	
 	$rs = mysql_query('show tables', $conn);	
 	while($row = mysql_fetch_array($rs, MYSQL_ASSOC)){
-		$data .= '###'.$row['Tables_in_'.$db].' (';
+		$data .= '### '.$row['Tables_in_'.$db];
 		$rs2 = mysql_query('SHOW TABLE STATUS', $conn);
+		
+		$comment = '';
 		while($row2 = mysql_fetch_array($rs2, MYSQL_ASSOC)){
 			if($row2['Name'] === $row['Tables_in_'.$db]){
-				$data .= $row2['Comment'].")\r\n";
+				$comment = $row2['Comment'];
 				break;
 			}
 		}
+		
+		if($comment){
+			$data .= " (".$comment.")\r\n\r\n";
+		} else {
+			$data .= "\r\n\r\n";
+		}
+		
 		$data .= "字段名|数据类型|是否为空|额外|注释\r\n---|---|---|---|---\r\n";
 		$rs1 = mysql_query('SHOW FULL COLUMNS FROM '.$row['Tables_in_'.$db], $conn);
 		while($row1 = mysql_fetch_array($rs1, MYSQL_ASSOC)){
 			$data .= $row1['Field'].'|'.$row1['Type'].'|'.($row1['Null']==='YES'?'是':'否').'|'.$row1['Extra'].'|'.$row1['Comment']."\r\n";
 		}
-		$data .= "\r\n#####索引\r\n";
+		$data .= "\r\n##### 索引\r\n";
 		
 		$index = array();
 		$rs3 = mysql_query('SHOW INDEX FROM '.$row['Tables_in_'.$db], $conn);
@@ -33,18 +46,25 @@ function mysql2md($host, $user, $pass, $db, $file){
 			$index[$row3['Key_name']]['index_type'] = $row3['Non_unique'] === '0' ? '是':'否';
 			$index[$row3['Key_name']]['type'] = $row3['Index_type'];
 		}
-		$data .= "\r\n索引名|是否唯一|索引类型|字段\r\n---|---|---|---\r\n";
-		foreach($index as $key_name => $key){
-			$data .= $key_name."|".$key['index_type']."|".$key['type']."|";
-			foreach($key['index_info'] as $value){
-				$data .= $value."<br>";
+		
+		if(is_array($index) && count($index) > 0){
+			$data .= "\r\n索引名|是否唯一|索引类型|字段\r\n---|---|---|---\r\n";
+			foreach($index as $key_name => $key){
+				$data .= $key_name."|".$key['index_type']."|".$key['type']."|";
+				foreach($key['index_info'] as $value){
+					$data .= $value."<br>";
+				}
+				$data = substr($data, 0, -4);
+				$data .= "\r\n";
 			}
-			$data = substr($data, 0, -4);
-			$data .= "\r\n";
+			$data .= "\r\n\r\n\r\n\r\n";
+		} else {
+			$data .= "\r\n\r\n无\r\n\r\n";
 		}
-		$data .= "\r\n\r\n\r\n\r\n";
-		echo "表 ".$row['Tables_in_'.$db]." 写入完成！\r\n";
+		echo "表 ".$row['Tables_in_'.$db]." 写入完成！\r\n\r\n";
 	}
+	
+	$data .= "最后修改时间  ".date('Y-m-d H:i:s')."\r\n\r\n";
 	
 	mysql_close($conn);
 	file_put_contents($file, $data);
