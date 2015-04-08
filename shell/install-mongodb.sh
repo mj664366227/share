cb0622435a58b7a4303bf4f53bb6a58b2ca0c560
ruanzhijun <ruanzhijun@kugou.net>
2015-04-08 14:24:19 +0800

    xx

--- shell/install-mongodb.sh
+++ shell/install-mongodb.sh
@@ -20,16 +20,32 @@ install_path='/install'
 rm -rf $install_path
 mkdir -p $install_path
 
+# 安装curl
+curl='curl-7.41.0'
+if [ ! -d $php_install_path/curl ]; then
+	echo 'installing '$curl' ...'
+	if [ ! -f $base_path/$curl.tar.gz ]; then
+		echo $curl'.tar.gz is not exists, system will going to download it...'
+		wget -O $base_path/$curl.tar.gz http://curl.haxx.se/download/$curl.tar.gz || exit
+		echo 'download '$curl' finished...'
+	fi
+	tar zxvf $base_path/$curl.tar.gz -C $install_path || exit
+	cd $install_path/$curl
+	./configure --prefix=$php_install_path/curl && make && make install || exit
+	yes|cp $php_install_path/curl/bin/* /usr/bin/
+	echo $curl' install finished...'
+fi
+
 bit=$(getconf LONG_BIT)
 if [ $bit = 32 ]; then
 	if [ ! -f $base_path/mongodb-$mongodb_version.tgz ]; then
-		wget -O $base_path/mongodb-$mongodb_version.tgz https://fastdl.mongodb.org/linux/mongodb-linux-i686-$mongodb_version.tgz || exit
+		curl -O $base_path/mongodb-$mongodb_version.tgz https://fastdl.mongodb.org/linux/mongodb-linux-i686-$mongodb_version.tgz || exit
 	fi
 	tar xzvf $base_path/mongodb-$mongodb_version.tgz -C $install_path || exit
 	mv $install_path/mongodb-linux-i686-$mongodb_version $mongodb_install_path/mongodb
 else
 	if [ ! -f $base_path/mongodb-$mongodb_version.tgz ]; then
-		wget -O $base_path/mongodb-$mongodb_version.tgz https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-$mongodb_version.tgz || exit
+		curl -O $base_path/mongodb-$mongodb_version.tgz https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-$mongodb_version.tgz || exit
 	fi
 	tar xzvf $base_path/mongodb-$mongodb_version.tgz -C $install_path || exit
 	mv $install_path/mongodb-linux-x86_64-$mongodb_version $mongodb_install_path/mongodb
