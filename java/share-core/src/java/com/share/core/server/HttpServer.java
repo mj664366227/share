18dec58cf4472d77127925591c29818cb48a3bf3
ruanzhijun <ruan965176201@qq.com>
2015-04-12 19:48:41 +0800

    xx

--- java/share-core/src/java/com/share/core/server/HttpServer.java
+++ java/share-core/src/java/com/share/core/server/HttpServer.java
@@ -73,6 +73,7 @@ public class HttpServer extends AbstractServer {
 	private String getWebappPath() {
 		for (String webappPath : webappPaths) {
 			File webappFile = new File((FileSystem.isWindows() ? "" : FileSystem.getSystemDir()) + webappPath, webXmlPath);
+			System.err.println(webappFile.getAbsolutePath());
 			if (webappFile.exists()) {
 				logger.warn("find " + webappFile.getAbsolutePath());
 				return webappPath;
