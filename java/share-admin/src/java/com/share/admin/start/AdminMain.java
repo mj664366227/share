ca155cf99a04919b77028953ab1cfcc413483f84
ruanzhijun <ruan965176201@qq.com>
2015-04-12 20:08:46 +0800

    xx

--- java/share-admin/src/java/com/share/admin/start/AdminMain.java
+++ java/share-admin/src/java/com/share/admin/start/AdminMain.java
@@ -1,13 +1,10 @@
 package com.share.admin.start;
 
+import com.share.core.server.HttpServer;
 import com.share.core.util.FileSystem;
 
 public class AdminMain {
 	public static void main(String[] a) {
-		JettyServer jettyServer = new JettyServer();
-		jettyServer.setPort(FileSystem.getPropertyInt("http.port"));
-		jettyServer.setContextPath("/");
-		jettyServer.start();
-		// new HttpServer(FileSystem.getPropertyInt("http.port")).start();
+		new HttpServer(FileSystem.getPropertyInt("http.port")).start();
 	}
 }
