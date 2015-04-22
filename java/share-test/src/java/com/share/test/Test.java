3afb2dcc4a0ef7975b894a66a15c1d063402b689
ruanzhijun <ruanzhijun@kugou.net>
2015-04-22 16:27:38 +0800

    xx

--- java/share-test/src/java/com/share/test/Test.java
+++ java/share-test/src/java/com/share/test/Test.java
@@ -1,6 +1,9 @@
 package com.share.test;
 
+import com.share.core.util.Ip;
+
 public class Test {
 	public static void main(String[] args) throws Exception {
+		System.err.println(Ip.ip2Long("0.0.0.0"));
 	}
 }
