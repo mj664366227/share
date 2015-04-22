75afc476c9d66234b81788191e0adce4628b1e53
ruanzhijun <ruanzhijun@kugou.net>
2015-04-22 17:23:54 +0800

    xx

--- java/compile.sh
+++ java/compile.sh
@@ -65,7 +65,8 @@ do
 	do
 		classpath="$classpath:""$jar"
 	done
-	mainClass='com.share.admin.start.AdminMain'
+	startName=$project
+	mainClass='com.share.'$startName'.start.'$startName'Main'
 	cmd='java -server -Xms128m -Xmx128m -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+PrintHeapAtGC -Xloggc:/srv/server/'$project'/log/gc.log -Dproject='$project' -classpath '$classpath' '$mainClass' >> /srv/server/'$project'/log/log.log'
 	echo $cmd > /srv/server/$project/run.sh
 	chmod 777 /srv/server/$project/run.sh
