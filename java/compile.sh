60ee80012fab49d52a7a5577d80e854da1ecd4bb
ruanzhijun <ruan965176201@qq.com>
2015-04-24 23:27:04 +0800

    x

--- java/compile.sh
+++ java/compile.sh
@@ -65,12 +65,12 @@ do
 	mainClass='com.share.'$startName'.start.'$(echo $className | tr '[a-z]' '[A-Z]')${startName:1}'Main'
 	
 	echo 'classpath="/srv/server/etc"
-for jar in `ls /srv/server/$project/lib/*.jar`
+for jar in `ls /srv/server/'$project'/lib/*.jar`
 do
 	classpath=$classpath:$jar
 done
 
-java -server -Xms128m -Xmx128m -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+PrintHeapAtGC -Xloggc:/srv/server/$project/log/gc.log -Dproject=$project -classpath $classpath $mainClass >> /srv/server/$project/log/log.log
+java -server -Xms128m -Xmx128m -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+PrintHeapAtGC -Xloggc:/srv/server/'$project'/log/gc.log -Dproject='$project' -classpath $classpath '$mainClass' >> /srv/server/'$project'/log/log.log
 '   > /srv/server/$project/run.sh
 	chmod 777 /srv/server/$project/run.sh
 done
