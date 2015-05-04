15118759e04b347e1cd3f9734594e2f766312f89
ruanzhijun <ruanzhijun@kugou.net>
2015-05-04 11:56:49 +0800

    xx

--- java/share-test/src/java/com/share/test/Test.java
+++ java/share-test/src/java/com/share/test/Test.java
@@ -1,9 +1,30 @@
 package com.share.test;
 
-import com.share.core.util.Ip;
+import java.util.HashMap;
+
+import com.share.core.util.RandomUtil;
+import com.share.core.util.Time;
+import com.share.test.timer.DemoTimer;
 
 public class Test {
 	public static void main(String[] args) throws Exception {
-		System.err.println(Ip.ip2Long("0.0.0.0"));
+		Class<?> cla = DemoTimer.class;
+		HashMap<String, String> map = new HashMap<String, String>();
+		int max = 5000;
+		for (int i = 0; i < max; i++) {
+			map.put(String.valueOf(System.nanoTime()+RandomUtil.rand(10000000, 999999999)), String.valueOf(System.nanoTime()));
+		}
+
+		long t = System.nanoTime();
+		for (int i = 0; i < max; i++) {
+			 cla.getMethod("demoTimer");
+		}
+		System.err.println(Time.showTime(System.nanoTime() - t));
+
+		t = System.nanoTime();
+		for (int i = 0; i < max; i++) {
+			map.get("a");
+		}
+		System.err.println(Time.showTime(System.nanoTime() - t));
 	}
 }
