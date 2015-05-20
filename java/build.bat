ce25ce510b852ffdef749b6b55622b0d62f2c85f
ruanzhijun <ruanzhijun@kugou.net>
2015-05-20 20:07:47 +0800

    xx

--- java/build.bat
+++ java/build.bat
@@ -1,28 +1,9 @@
 ﻿@echo off
 rd /s/q %cd%\.gradle\
 
-set PROJECT=share-core
-rd /s/q %cd%\%PROJECT%\.settings
-rd /s/q %cd%\%PROJECT%\bin
-del /s/q %cd%\%PROJECT%\.classpath
-del /s/q %cd%\%PROJECT%\.project
+cd %cd%
+for /f "tokens=*" %%a in ('dir /b') do (
+   echo %%a | findstr "\-" && del /s /q %cd%\%%a\.classpath && del /s /q %cd%\%%a\.project && rd /s /q %cd%\%%a\.settings && rd /s /q %cd%\%%a\bin || echo ...
+)
 
-set PROJECT=share-client
-rd /s/q %cd%\%PROJECT%\.settings
-rd /s/q %cd%\%PROJECT%\bin
-del /s/q %cd%\%PROJECT%\.classpath
-del /s/q %cd%\%PROJECT%\.project
-
-set PROJECT=share-test
-rd /s/q %cd%\%PROJECT%\.settings
-rd /s/q %cd%\%PROJECT%\bin
-del /s/q %cd%\%PROJECT%\.classpath
-del /s/q %cd%\%PROJECT%\.project
-
-set PROJECT=share-admin
-rd /s/q %cd%\%PROJECT%\.settings
-rd /s/q %cd%\%PROJECT%\bin
-del /s/q %cd%\%PROJECT%\.classpath
-del /s/q %cd%\%PROJECT%\.project
-
-gradle eclipse
+gradle clean eclipse
