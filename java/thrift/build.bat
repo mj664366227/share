8ed8b732f67ad922b8763e17fa447980b8540350
ruanzhijun <ruan965176201@qq.com>
2015-05-23 15:38:20 +0800

    xx

--- java/thrift/build.bat
+++ java/thrift/build.bat
@@ -1,11 +1,9 @@
 ﻿@echo off
 set thrift=%cd%\thrift-0.9.2.exe
 
-rd /s /q ..\..\share-soa\src\java
+rd /s /q %cd%\..\share-soa\src\java\com\share\soa\thrift
 
 cd %cd%\share
 for /f "tokens=*" %%a in ('dir /b') do (
 	echo %%a | findstr "\.thrift" && %thrift% -r --gen java -out ..\..\share-soa\src\java %cd%\%%a || cd %cd%
 )
-
-pause
--- java/thrift/share/test.thrift
+++ java/thrift/share/test.thrift
@@ -1,14 +1,15 @@
 namespace java com.share.soa.thrift
 
-struct  NewsModel{
-	1:i32 id ;
-	2:string title;
+struct  ShareObject{
+	1:i32 id1;
+	2:i16 id2;
 	3:string content;
 	4:string media_from;
 	5:string author;
 }
 
-service IndexNewsOperatorServices {
-	bool indexNews(1:NewsModel indexNews),
-	bool deleteArtificiallyNews(1:i32 id )
+#注释 ...
+service testShareObject {
+	i32 test(1:i32 a),
+	string testString(1:i32 n,2:bool aaa)
 }
