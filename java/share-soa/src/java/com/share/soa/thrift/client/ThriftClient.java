d5a6d798ed736e7b9a64a3ccc1aa4ac44767f3e2
ruanzhijun <ruan965176201@qq.com>
2015-05-23 16:13:21 +0800

    xx

--- java/share-soa/src/java/com/share/soa/thrift/client/ThriftClient.java
+++ java/share-soa/src/java/com/share/soa/thrift/client/ThriftClient.java
@@ -8,9 +8,17 @@ import org.apache.thrift.transport.TTransport;
 import com.share.soa.thrift.protocol.TestShareObject;
 
 public class ThriftClient {
-	TTransport transport = new TSocket("127.0.0.1", 9813);
-	long start = System.currentTimeMillis();
-	TProtocol protocol = new TBinaryProtocol(transport);
 
-	TestShareObject.Client client = new TestShareObject.Client(protocol);
+	public static void main(String[] aaa) throws Exception {
+		TTransport transport = new TSocket("127.0.0.1", 9813);
+		TProtocol protocol = new TBinaryProtocol(transport);
+
+		TestShareObject.Client client = new TestShareObject.Client(protocol);
+
+		transport.open();
+
+		client.test(1);
+
+		transport.close();
+	}
 }
