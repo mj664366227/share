b121c1ccabe33559de80943ea66975b5cccc3a4f
ruanzhijun <ruan965176201@qq.com>
2015-05-23 15:43:15 +0800

    xx

--- java/thrift/build.bat
+++ java/thrift/build.bat
@@ -5,5 +5,5 @@ rd /s /q %cd%\..\share-soa\src\java\com\share\soa\thrift\model
 
 cd %cd%\share
 for /f "tokens=*" %%a in ('dir /b') do (
-	echo %%a | findstr "\.thrift" && %thrift% -r --gen java -out ..\..\share-soa\src\java %cd%\%%a || cd %cd%
+	echo %%a | findstr ".thrift" && %thrift% -r --gen java -out ..\..\share-soa\src\java %cd%\%%a || cd %cd%
 )
