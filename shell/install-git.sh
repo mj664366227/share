db56554e2fddcdd70b6146c1793852cd14e38ba6
ruanzhijun <ruan965176201@qq.com>
2015-08-17 21:32:26 +0800

    xx

--- shell/install-git.sh
+++ shell/install-git.sh
@@ -18,19 +18,19 @@ echo 'preparing working path...'
 install_path='/install'
 rm -rf $install_path
 mkdir -p $install_path
+
+yum -y install curl curl-devel zlib-devel autoconf openssl-devel perl cpio expat-devel gettext-devel gcc libc6-dev gcc-c++ pcre-devel libgd2-xpm libgd2-xpm-dev geoip-database libgeoip-dev make libxslt-dev rsync lrzsz
+
 cd $install_path
-
-yum -y install curl curl-devel zlib-devel openssl-devel perl cpio expat-devel gettext-devel gcc libc6-dev gcc-c++ pcre-devel libgd2-xpm libgd2-xpm-dev geoip-database libgeoip-dev make libxslt-dev rsync lrzsz
-
 if [ ! -f $base_path/git-master.zip ]; then
 	echo 'git.zip is not exists, system will going to download it...'
 	wget -O $base_path/git-master.zip https://github.com/git/git/archive/master.zip || exit
 	echo 'download git.zip finished...'
 fi
-unzip git-master.zip || exit
+unzip $base_path/git-master.zip -d $install_path || exit
 
 cd $install_path/git-master
-./configure --prefix=$git_install_path/git && make && make install || exit 
+autoconf && ./configure --prefix=$git_install_path/git && make && make install || exit 
 yes|cp -rf $git_install_path/git/bin/* /usr/bin/
 git --version
 
