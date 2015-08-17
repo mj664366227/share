b115237d04cb0ac18983aa179769d9941d53b494
ruanzhijun <ruan965176201@qq.com>
2015-08-17 17:45:04 +0800

    xx

--- shell/install-gitlab.sh
+++ shell/install-gitlab.sh
@@ -1,9 +1,18 @@
 #linux git
-#运行例子：sh install-gitlab.sh /usr/local
+#运行例子：sh install-gitlab.sh
  
 #定义本程序的当前目录
 base_path=$(pwd)
 ntpdate time.nist.gov
+
+#建立临时安装目录
+echo 'preparing working path...'
+install_path='/install'
+rm -rf $install_path
+mkdir -p $install_path
+cd $install_path
+
+yum -y install libtool sed gcc gcc-c++ make net-snmp net-snmp-devel net-snmp-utils libc6-dev python-devel rsync perl bc lrzsz git
 
 #安装EPEL扩展源
 rpm -ivh http://fr2.rpmfind.net/linux/epel/6/x86_64/epel-release-6-8.noarch.rpm
@@ -12,5 +21,5 @@ rpm -ivh http://fr2.rpmfind.net/linux/epel/6/x86_64/epel-release-6-8.noarch.rpm
 git clone https://github.com/mattias-ohlsson/gitlab-installer.git
 
 #执行gitlab安装脚本
-wget https://github.com/mattias-ohlsson/gitlab-installer/blob/master/gitlab-install-el6.sh || exit
-sh ./gitlab-install-el6.sh || exit
+cd $install_path/gitlab-installer
+sh gitlab-install-el6.sh || exit
