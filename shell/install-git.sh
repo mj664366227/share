9c553273a0b19bca6602efea19a35a69ecf1121b
ruanzhijun <ruan965176201@qq.com>
2015-08-17 21:35:38 +0800

    hfgh

--- shell/install-git.sh
+++ shell/install-git.sh
@@ -19,9 +19,21 @@ install_path='/install'
 rm -rf $install_path
 mkdir -p $install_path
 
-yum -y install curl curl-devel zlib-devel autoconf openssl-devel perl cpio expat-devel gettext-devel gcc libc6-dev gcc-c++ pcre-devel libgd2-xpm libgd2-xpm-dev geoip-database libgeoip-dev make libxslt-dev rsync lrzsz
+yum -y install curl curl-devel zlib-devel openssl-devel perl cpio expat-devel gettext-devel gcc libc6-dev gcc-c++ pcre-devel libgd2-xpm libgd2-xpm-dev geoip-database libgeoip-dev make libxslt-dev rsync lrzsz
 
 cd $install_path
+
+if [ ! -d $git_install_path/autoconf ]; then
+	autoconf='autoconf-2.69'
+	if [ ! -f $base_path/$autoconf.tar.gz ]; then
+		wget -O $base_path/$autoconf.tar.gz http://ftp.gnu.org/gnu/autoconf/$autoconf.tar.gz || exit
+	fi
+	tar zxvf $base_path/$autoconf.tar.gz -C $install_path || exit
+	cd $install_path/$autoconf
+	./configure --prefix=$git_install_path/autoconf && make && make install || exit
+	yes|cp $git_install_path/autoconf/bin/* /usr/bin/
+fi
+
 if [ ! -f $base_path/git-master.zip ]; then
 	echo 'git.zip is not exists, system will going to download it...'
 	wget -O $base_path/git-master.zip https://github.com/git/git/archive/master.zip || exit
