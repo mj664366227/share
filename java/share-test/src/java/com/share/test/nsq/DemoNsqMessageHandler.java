96f4391894d09ca4e63680e3c8303c77523bfaea
ruanzhijun <ruan965176201@qq.com>
2015-11-21 16:47:58 +0800

    czx

--- java/share-test/src/java/com/share/test/nsq/DemoNsqMessageHandler.java
+++ java/share-test/src/java/com/share/test/nsq/DemoNsqMessageHandler.java
@@ -4,10 +4,11 @@ import com.share.core.interfaces.NsqMessageHandler;
 
 public class DemoNsqMessageHandler implements NsqMessageHandler {
 	//@NsqCallback(topic = "a", channel = "a", onlyChannel = false)
-	public void handle(byte[] message) {
+	public boolean handle(byte[] message) {
 		// 先getMessage()
 		// 然后做你想要做的事情
 		// 最后finished()
 		System.err.println("xxx收到消息：  " + new String(message));
+		return true;
 	}
 }
