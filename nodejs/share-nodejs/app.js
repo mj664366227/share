5228b3489477a96ddaa5113d0fabd83d2fb0566d
ruanzhijun <ruan965176201@qq.com>
2015-12-09 17:19:08 +0800

    fsd

--- nodejs/share-nodejs/app.js
+++ nodejs/share-nodejs/app.js
@@ -1,5 +1,3 @@
 var h = require('./core/core'); 
-h.hello('snandy');
-console.log(h.area(12));
-console.log(h.config);
-console.log(processor.argv);
+var filesystem = require('./core/util/filesystem'); 
+console.log(filesystem.getSystemDir());
---
+++ nodejs/share-nodejs/core/util/filesystem.js
@@ -0,0 +1,4 @@
+// 获取系统绝对路径
+exports.getSystemDir = function () {
+    return __dirname;
+};
