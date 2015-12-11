21747033768b980107d0923297c4e0431c0acb1e
ruanzhijun <ruan965176201@qq.com>
2015-12-11 12:19:08 +0800

    das

--- nodejs/share-nodejs/app.js
+++ nodejs/share-nodejs/app.js
@@ -1,8 +1,16 @@
 var http = require('http');
-http.createServer(function handler(req, res) {
-	res.writeHead(200, {
-		'Content-Type' : 'text/plain'
-	});
-	res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
+var databaseConfig = require('./config/database');
+
+
+exports.upload1 = function() {}
+exports.upload2 = function() {}
+exports.upload3 = function() {}
+exports.upload4 = function() {}
+console.log(module);
+// http.createServer(function handler(req, res) {
+// res.writeHead(200, {
+// 'Content-Type' : 'text/plain'
+// });
+// res.end('Hello World\n');
+// }).listen(1337, '127.0.0.1');
+// console.log('Server running at http://127.0.0.1:1337/');
