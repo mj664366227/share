077b9a7cc2be71bade6fb5151a006db24dda752d
ruanzhijun <ruan965176201@qq.com>
2015-12-11 14:08:04 +0800

    dsad

--- nodejs/share-nodejs/app.js
+++ nodejs/share-nodejs/app.js
@@ -1,30 +1,42 @@
 var http = require('http');
 var mysql = require('mysql');
 var log4js = require('log4js');
+var base64 = require('./core/util/base64');
 var databaseConfig = require('./config/database');
 
-// logger configure
+var log4js = require('log4js');
 log4js.configure({
 	appenders : [ {
-		type : 'console'
-	}, {
-		type : 'dateFile',
-		filename : 'log/pattern_yyyy-MM-dd.log',
-		// filename: "blah.log",
-		pattern : "yyyy-MM-dd",
-
-		maxLogSize : 1024,
-		// "pattern": "-yyyy-MM-dd",
-		alwaysIncludePattern : false,
-
-		backups : 4,
-		category : 'normal'
-	}, ],
-	replaceConsole : true
+		type : 'console',
+		layout : {
+			type : 'pattern',
+			pattern : '%d{yyyy-MM-dd hh:mm:ss} [%-4p] %c - %m',
+			absolute : true,
+			tokens : {
+				pid : function() {
+					return module.filename;
+				}
+			}
+		}
+	}, // 控制台输出
+	{
+		type : 'file', // 文件输出
+		filename : 'log/log.log',
+		maxLogSize : 10000000
+	} ],
+	levels : {
+		dateFileLog : 'info',
+		console : 'info'
+	}
 });
+var logger = log4js.getLogger('core.util.base64');
+logger.trace('Entering cheese testing');
+logger.debug('Got cheese.');
+logger.info('Cheese is Gouda.');
+logger.warn('Cheese is quite smelly.');
+logger.error('Cheese is too ripe!');
+logger.fatal('Cheese was breeding ground for listeria.');
 
-log4js.getLogger(databaseConfig).info("测试出版司法官if孤独");
-log4js.getLogger(databaseConfig).warn("sssssssss");
 // http.createServer(function handler(req, res) {
 // res.writeHead(200, {
 // 'Content-Type' : 'text/plain'
