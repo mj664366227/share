6f984a09ed7eaa33a6b549e10f52b295ac99c2ed
ruanzhijun <ruan965176201@qq.com>
2015-12-11 11:13:16 +0800

    xsa

--- nodejs/share-nodejs/app.js
+++ nodejs/share-nodejs/app.js
@@ -1,29 +1,8 @@
-
-var filesystem = require('./core/util/filesystem');
-var httpServer = require('./core/server/httpServer');
-var demoController = require('./controller/demoController');
-var config = require('./config/config');
-var database = require('./config/database');
-var base64 = require('./core/util/base64');
-
-var requestMapping = {};
-requestMapping["/start"] = demoController.start;
-requestMapping["/upload"] = demoController.upload;
-for ( var fun in requestMapping) {
-	console.log("requestMapping: " + fun);
-}
-
-//core.init();
-
-// httpServer.start(9999, requestMapping);
-
-var mysql = require("mysql");
-var pool = mysql.createPool(database.config);
-pool.getConnection(function(err, connection) {
-	connection.query('SELECT * FROM `user`', function(err, rows) {
-		connection.release();
-		for (var i = 0; i < rows.length; i++) {
-			// console.log(rows[i].nickname);
-		}
+var http = require('http');
+http.createServer(function handler(req, res) {
+	res.writeHead(200, {
+		'Content-Type' : 'text/plain'
 	});
-});
+	res.end('Hello World\n');
+}).listen(1337, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337/');
