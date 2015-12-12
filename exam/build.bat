52f3c22d9ed7bbebc6d81de8040b70f77cadb5b7
ruanzhijun <ruan965176201@qq.com>
2015-12-12 16:14:08 +0800

    dasa

--- exam/build.bat
+++ exam/build.bat
@@ -1,9 +1,16 @@
 ﻿@echo off
-rd /s/q %cd%\.gradle\
+
+
+
+rd /s/q %cd%\lib\
+md %cd%\lib\
+
+
 
 cd %cd%
 for /f "tokens=*" %%a in ('dir /b') do (
    echo %%a | findstr "\-" && del /s /q %cd%\%%a\.classpath && del /s /q %cd%\%%a\.project && rd /s /q %cd%\%%a\.settings && rd /s /q %cd%\%%a\bin || cd %cd%
 )
 
-gradle clean eclipse 
+:gradle clean eclipse 
+gradle clean deploy && copy /y %cd%\exam\compile\* %cd%\lib\ && rd /s/q %cd%\.gradle\ && rd /s/q %cd%\exam\build\ && rd /s/q %cd%\exam\compile\ && rd /s/q %cd%\exam\.settings\ && rd /s/q %cd%\exam\bin\ && del /s/q %cd%\exam\.classpath && del /s/q %cd%\exam\.project
