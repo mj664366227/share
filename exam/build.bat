e628141d9405289cf275273a44aa5e76cb402835
ruanzhijun <ruan965176201@qq.com>
2016-01-11 23:08:31 +0800

    ss

--- exam/build.bat
+++ exam/build.bat
@@ -14,5 +14,5 @@ for /f "tokens=*" %%a in ('dir /b') do (
    echo %%a | findstr "\-" && del /s /q %cd%\%%a\.classpath && del /s /q %cd%\%%a\.project && rd /s /q %cd%\%%a\.settings && rd /s /q %cd%\%%a\bin || cd %cd%
 )
 
-:gradle clean eclipse 
-gradle clean deploy && xcopy /y %cd%\exam\compile\* D:\exam\lib\ /e && rd /s/q %cd%\.gradle\ && rd /s/q %cd%\exam\build\ && rd /s/q %cd%\exam\compile\ && xcopy /y %cd%\exam\src\resources\* D:\exam\etc\ /e && rd /s/q %cd%\exam\.settings\ && rd /s/q %cd%\exam\bin\ && del /s/q %cd%\exam\.classpath && del /s/q %cd%\exam\.project
+gradle clean eclipse 
+:gradle clean deploy && xcopy /y %cd%\exam\compile\* D:\exam\lib\ /e && rd /s/q %cd%\.gradle\ && rd /s/q %cd%\exam\build\ && rd /s/q %cd%\exam\compile\ && xcopy /y %cd%\exam\src\resources\* D:\exam\etc\ /e && rd /s/q %cd%\exam\.settings\ && rd /s/q %cd%\exam\bin\ && del /s/q %cd%\exam\.classpath && del /s/q %cd%\exam\.project
