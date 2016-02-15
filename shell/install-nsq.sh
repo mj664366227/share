040d969d9fd18ba7bdc6a113b379748305a79d18
ruanzhijun <ruan965176201@qq.com>
2016-02-15 14:36:05 +0800

    xx

--- shell/install-nsq.sh
+++ shell/install-nsq.sh
@@ -31,25 +31,25 @@ chmod -R 777 $nsq_install_path/nsq/bin/*
 yes | cp -rf $nsq_install_path/nsq/bin/* /usr/bin/
 
 #生成启动nsq脚本
-rm -rf $nsq_install_path/nsq/start_nsq.sh
-echo 'cd '$nsq_install_path'/nsq/' >> $nsq_install_path/nsq/start_nsq.sh
+rm -rf $nsq_install_path/nsq/start.sh
+echo 'cd '$nsq_install_path'/nsq/' >> $nsq_install_path/nsq/start.sh
 ifconfig > 'tmp'
 temp=$(awk '{print;exit}' tmp | cut -f 1 -d ":")
-echo "#!/bin/bash" >> $nsq_install_path/nsq/start_nsq.sh
-echo "list=\$(ps -ef|grep nsq|grep start_nsq|grep -v grep|awk '{print \$2}')" >> $nsq_install_path/nsq/start_nsq.sh
-echo "for proc in \$list" >> $nsq_install_path/nsq/start_nsq.sh
-echo "do" >> $nsq_install_path/nsq/start_nsq.sh
-echo "	kill -15 \$proc" >> $nsq_install_path/nsq/start_nsq.sh
-echo "done" >> $nsq_install_path/nsq/start_nsq.sh
-echo 'ip=$(ifconfig '$temp' | grep "netmask" | cut -b 14-26)' >> $nsq_install_path/nsq/start_nsq.sh
-echo 'nsqlookupd &' >> $nsq_install_path/nsq/start_nsq.sh
-echo 'sleep 2' >> $nsq_install_path/nsq/start_nsq.sh
-echo 'nsqd --lookupd-tcp-address=$ip:4160 --broadcast-address=$ip &' >> $nsq_install_path/nsq/start_nsq.sh
-echo 'sleep 2' >> $nsq_install_path/nsq/start_nsq.sh
-echo 'nsqadmin --lookupd-http-address=$ip:4161 -http-address=$ip:4171 &' >> $nsq_install_path/nsq/start_nsq.sh
-echo 'sleep 2' >> $nsq_install_path/nsq/start_nsq.sh
-echo 'nsq_to_file --output-dir='$nsq_install_path'/nsq/data --lookupd-http-address=$ip:4161 &' >> $nsq_install_path/nsq/start_nsq.sh
-chmod 777 $nsq_install_path/nsq/start_nsq.sh
+echo "#!/bin/bash" >> $nsq_install_path/nsq/start.sh
+echo "list=\$(ps -ef|grep nsq|grep start|grep -v grep|awk '{print \$2}')" >> $nsq_install_path/nsq/start.sh
+echo "for proc in \$list" >> $nsq_install_path/nsq/start.sh
+echo "do" >> $nsq_install_path/nsq/start.sh
+echo "	kill -15 \$proc" >> $nsq_install_path/nsq/start.sh
+echo "done" >> $nsq_install_path/nsq/start.sh
+echo 'ip=$(ifconfig '$temp' | grep "netmask" | cut -b 14-26)' >> $nsq_install_path/nsq/start.sh
+echo 'nsqlookupd &' >> $nsq_install_path/nsq/start.sh
+echo 'sleep 2' >> $nsq_install_path/nsq/start.sh
+echo 'nsqd --lookupd-tcp-address=$ip:4160 --broadcast-address=$ip &' >> $nsq_install_path/nsq/start.sh
+echo 'sleep 2' >> $nsq_install_path/nsq/start.sh
+echo 'nsqadmin --lookupd-http-address=$ip:4161 -http-address=$ip:4171 &' >> $nsq_install_path/nsq/start.sh
+echo 'sleep 2' >> $nsq_install_path/nsq/start.sh
+echo 'nsq_to_file --output-dir='$nsq_install_path'/nsq/data --lookupd-http-address=$ip:4161 &' >> $nsq_install_path/nsq/start.sh
+chmod 777 $nsq_install_path/nsq/start.sh
 rm -rf 'tmp'
 
-$nsq_install_path/nsq/start_nsq.sh
+$nsq_install_path/nsq/start.sh
