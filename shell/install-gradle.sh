d9093caca1f5ba1582c386748181bb42a04f5dce
ruanzhijun <ruan965176201@qq.com>
2016-05-15 12:20:32 +0800

    xx

--- shell/install-gradle.sh
+++ shell/install-gradle.sh
@@ -20,7 +20,7 @@ rm -rf $install_path
 mkdir -p $install_path
 
 #下载gradle
-gradle='gradle-2.11'
+gradle='gradle-2.13'
 if [ ! -d $install_path/$gradle ]; then
 	echo 'installing '$gradle' ...'
 	if [ ! -f $base_path/$gradle.zip ]; then
