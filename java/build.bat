6e028ba264990b70262a19acfb3d6b5260ebd091
ruanzhijun <ruan965176201@qq.com>
2016-05-18 08:34:20 +0800

    xx

--- java/build.bat
+++ java/build.bat
@@ -3,7 +3,16 @@ rd /s/q %cd%\.gradle\
 
 cd %cd%
 for /f "tokens=*" %%a in ('dir /b') do (
-   echo %%a | findstr "\-" && del /s /q %cd%\%%a\.classpath && del /s /q %cd%\%%a\.project && rd /s /q %cd%\%%a\.settings && rd /s /q %cd%\%%a\bin || cd %cd%
+   echo %%a | findstr "\-" && del /s /q %cd%\%%a\.classpath || cd %cd%
+)
+for /f "tokens=*" %%a in ('dir /b') do (
+   echo %%a | findstr "\-" && del /s /q %cd%\%%a\.project || cd %cd%
+)
+for /f "tokens=*" %%a in ('dir /b') do (
+   echo %%a | findstr "\-" && rd /s /q %cd%\%%a\.settings || cd %cd%
+)
+for /f "tokens=*" %%a in ('dir /b') do (
+   echo %%a | findstr "\-" && rd /s /q %cd%\%%a\bin || cd %cd%
 )
 
 gradle clean eclipse
