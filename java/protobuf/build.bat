cfb4c896c5606ba6b2fb47f645d62bc8e0aab7f7
ruanzhijun <ruan965176201@qq.com>
2016-05-21 11:15:07 +0800

    zx

--- java/protobuf/build.bat
+++ java/protobuf/build.bat
@@ -1,2 +1,2 @@
-rd /s /q %cd%\..\share-core\src\com
-%cd%\protoc\protoc.exe --java_out=%cd%\..\share-core\src -I=%cd%\pb %cd%\pb\*.proto
+rd /s /q %cd%\..\share-core\src\java\com\share\core\protocol\protobuf
+%cd%\protoc\protoc.exe --java_out=%cd%\..\share-core\src\java\ -I=%cd%\pb %cd%\pb\*.proto
