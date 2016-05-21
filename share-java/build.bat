a36adee5f0089befc9c374abda3b64b4a4f3c276
ruanzhijun <ruan965176201@qq.com>
2016-05-22 00:27:20 +0800

    xx

--- share-java/build.bat
+++ share-java/build.bat
@@ -1,6 +1,6 @@
 @echo off
 
-:处理pb
+
 rd /s /q %cd%\share-core\src\java\com\share\core\protocol\protobuf
 %cd%\protobuf\protoc\protoc.exe --java_out=%cd%\share-core\src\java\ -I=%cd%\protobuf\pb %cd%\protobuf\pb\*.proto
 
