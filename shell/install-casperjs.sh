5f40468ca3abea74bc205f6b8959249018a5169d
ruanzhijun <ruan965176201@qq.com>
2016-07-29 23:32:29 +0800

    xxx

--- shell/install-casperjs.sh
+++ shell/install-casperjs.sh
@@ -6,8 +6,9 @@ base_path=$(pwd)
 ntpdate ntp.api.bz
 
 #处理外部参数
-casperjs_install_path=$1
-if [ ! $casperjs_install_path ]; then
+casperjs_install_version=$1
+casperjs_install_path=$2
+if [ ! $casperjs_install_version ] || [ ! $casperjs_install_path ]; then
 	echo 'error command!!! you must input casperjs version and install path...'
 	echo 'for example: sh install-casperjs.sh 1.1.3 /usr/local'
 	exit
@@ -20,7 +21,7 @@ rm -rf $install_path
 mkdir -p $install_path
 
 #下载casperjs
-casperjs='casperjs-1.1.3'
+casperjs='casperjs-'$casperjs_install_version
 echo 'installing '$casperjs' ...'
 if [ ! -f $base_path/$casperjs.zip ]; then
 	echo $casperjs'.zip is not exists, system will going to download it...'
