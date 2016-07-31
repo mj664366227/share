23f5dc6be204a54ced395b5b37024865ae665530
ruanzhijun <ruan965176201@qq.com>
2016-07-31 20:19:43 +0800

    研究log4j2

--- share-java/share-test/src/java/com/share/test/Test.java
+++ share-java/share-test/src/java/com/share/test/Test.java
@@ -1,7 +1,22 @@
 package com.share.test;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.share.core.exception.HttpRequestMethodNotDefineException;
+
 public class Test {
-	public static void main(String[] args) throws Exception {
-		System.err.println(Math.round(7.5));
+	private static final Logger logger = LogManager.getLogger(Test.class);
+
+	public static void main(final String... args) {
+
+		// Set up a simple configuration that logs on the console.
+
+		logger.trace("trace........");
+		logger.info("info...........", new HttpRequestMethodNotDefineException("xxxxxxxx"));
+		logger.debug("debug..........");
+		logger.error("error.............");
+		logger.warn("warn..........");
+		logger.fatal("Exiting application  {}", "广东省各地");
 	}
 }
