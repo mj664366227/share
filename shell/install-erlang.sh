1c71fe34543437a86aec04dad970533ba0b402db
ruanzhijun <ruan965176201@qq.com>
2016-11-17 14:03:16 +0800

    dd

--- shell/install-erlang.sh
+++ shell/install-erlang.sh
@@ -22,7 +22,7 @@ rm -rf $install_path
 mkdir -p $install_path
 
 # 安装OpenSSL
-openssl='openssl-1.1.0c'
+openssl='openssl-1.0.2j'
 if [ ! -d $erlang_install_path/openssl ]; then
 	echo 'installing '$openssl' ...'
 	if [ ! -f $base_path/$openssl.tar.gz ]; then
