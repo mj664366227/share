10c369aeb7940c91d706c217bcd235f2fd2b70da
ruanzhijun <ruan965176201@qq.com>
2016-12-25 21:27:59 +0800

    xx

--- share-node/build.bat
+++ share-node/build.bat
@@ -1,15 +1,7 @@
 @echo off
 
-set DIR=%cd%
-cd %DIR%
-:先备份core工程的package.json
-xcopy %DIR%\share-core\package.json %DIR%\ /y
-
-:删除所有老文件
-for /f "tokens=*" %%a in ('dir /b') do (
-   echo %%a | findstr "share\-" && del /f /q %DIR%\%%a\package.json && xcopy %DIR%\package.json %DIR%\%%a\ /y || cd %DIR%
-)
-
 :npm部署
-rd /s /q %DIR%\node_modules
-npm install && del /f /q %DIR%\package.json
+rd /s /q %cd%\app
+rd /s /q %cd%\runtime
+rd /s /q %cd%\node_modules
+npm install && npm run compile
