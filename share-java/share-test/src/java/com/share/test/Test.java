a7b7ab8516237dca08b2276990896e353c431163
ruanzhijun <ruan965176201@qq.com>
2017-01-08 21:05:50 +0800

    dd

--- share-java/share-test/src/java/com/share/test/Test.java
+++ share-java/share-test/src/java/com/share/test/Test.java
@@ -1,37 +1,7 @@
 package com.share.test;
 
-import java.io.File;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.share.core.util.FileSystem;
-
 public class Test {
 
-	//第二类  绝对路径的配置文件
-	public static void test1() {
-		String path = FileSystem.getSystemDir() + ".." + File.separator + ".." + File.separator + "share-java" + File.separator + "share-core" + File.separator + "bin" + File.separator;
-		System.err.println(path);
-		//System.err.println(FileSystem.getSystemDir()+"../../share-java\share-core\bin");
-		//我们将log4j2.xml放在D盘下
-		//这是需要手动的加载
-		//绝对路径配置文件		
-		Logger logger = LoggerFactory.getLogger(Test.class.getName());
-		logger.trace("trace...");
-		logger.debug("debug...");
-		logger.info("info...");
-		logger.warn("warn...");
-		logger.error("error...");
-		//一下是运行效果
-		/*2014-09-01 16:03:07,331 DEBUG [main] test.ConfigTest (ConfigTest.java:42) - debug...
-		2014-09-01 16:03:07,331 INFO  [main] test.ConfigTest (ConfigTest.java:43) - info...
-		2014-09-01 16:03:07,331 WARN  [main] test.ConfigTest (ConfigTest.java:44) - warn...
-		2014-09-01 16:03:07,331 ERROR [main] test.ConfigTest (ConfigTest.java:45) - error...
-		2014-09-01 16:03:07,331 FATAL [main] test.ConfigTest (ConfigTest.java:46) - fatal...*/
-	}
-
 	public static void main(String[] args) throws InterruptedException {
-		test1();
 	}
 }
--- share-java/share-test/src/java/com/share/test/junit/JunitTest.java
+++ share-java/share-test/src/java/com/share/test/junit/JunitTest.java
@@ -1,19 +1,35 @@
 package com.share.test.junit;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.share.core.component.JwtService;
+import com.share.core.util.JSONObject;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:share-test.xml" })
 public class JunitTest {
-	private final static Logger logger = LogManager.getLogger(JunitTest.class);
+	private final static Logger logger = LoggerFactory.getLogger(JunitTest.class);
+
+	@Autowired
+	private JwtService jwtService;
 
 	@Test
 	public void junitTest() throws Exception {
-		logger.info("dsdd");
+		JSONObject data = new JSONObject();
+		data.put("a", 1);
+		data.put("b", 2);
+		data.put("c", 3);
+		data.put("d", 4);
+		System.err.println(data);
+		String encode = jwtService.sign(data);
+		System.err.println("encode: " + encode);
+		JSONObject json = jwtService.verify(encode);
+		System.err.println(json);
 	}
 }
